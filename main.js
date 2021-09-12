7b2bf618f1627eaad0b561cdb1ca78be0dc97818
Horie <17owlet@gmail.com>
2021-09-12 12:02:05 +0900

    slickできたところまで

--- main.js
+++ main.js
@@ -12,4 +12,32 @@ jQuery(function($) {
     });
     });
 
-$('.slider').slick();
+    
+// $('.slider').slick();
+
+$('.slider').slick({
+    autoplay:false,
+    // autoplaySpeed:5000,
+    dots:true,
+    slidesToShow:4,
+    responsive:[
+        {
+            breakpoint: 1024,
+            settings:{
+                slidesToShow:3,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings:{
+                slidesToShow:2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings:{
+                slidesToShow:1,
+            }
+        },
+    ]
+});
