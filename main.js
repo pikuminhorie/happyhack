c898ec6b8face209164c64a9e0c7cef528b56970
Horie <17owlet@gmail.com>
2021-09-13 00:52:54 +0900

    一応完成…？> <

--- main.js
+++ main.js
@@ -42,3 +42,16 @@ $('.slider').slick({
     ]
 });
 
+$(document).ready(function(){
+	$('.fuwat').css('visibility','hidden');
+	$(window).scroll(function(){
+	 var windowHeight = $(window).height(),
+		 topWindow = $(window).scrollTop();
+	 $('.fuwat').each(function(){
+	  var objectPosition = $(this).offset().top;
+	  if(topWindow > objectPosition - windowHeight + 200){
+	   $(this).addClass("fuwatAnime");
+	  }
+	 });
+	});
+  });
