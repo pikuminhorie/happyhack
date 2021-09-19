599e39f6604cf52ab313b3c389ab6f625aa977e5
Horie <17owlet@gmail.com>
2021-09-19 23:19:01 +0900

    とりあえず消えてたの直せた

--- main.js
+++ main.js
@@ -21,24 +21,24 @@ $('.slider').slick({
     dots:true,
     slidesToShow:4,
     responsive:[
-        {
-            breakpoint: 1024,
-            settings:{
-                slidesToShow:3,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings:{
-                slidesToShow:2,
-            }
-        },
+        // {
+        //     breakpoint: 1024,
+        //     settings:{
+        //         slidesToShow:3,
+        //     }
+        // },
+        // {
+        //     breakpoint: 768,
+        //     settings:{
+        //         slidesToShow:2,
+        //     }
+        // },
         {
             breakpoint: 480,
             settings:{
                 slidesToShow:1,
             }
-        },
+        }
     ]
 });
 
